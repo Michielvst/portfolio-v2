27de21f5a3c955c5a6780e66126824f85eb6a4ca
Michiel Van Steen <michiel--vs@outlook.com>
2020-06-26 13:17:38 +0100

    a

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -37,7 +37,7 @@ export default function Contact() {
               name="contact"
               className="button resumeButton"
             >
-              Resume
+              Resumes
             </Button>{" "}
           </a>
         </div>
